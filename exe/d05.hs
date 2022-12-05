217f42913b5235f44b70c27bb6bd13bfbf41f398
Cédric B <cedric@arch.localdomain>
2022-12-05 23:14:01 +0100

    day 5 part 1

---
+++ exe/d05.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import D05.Solution (solve)
+
+main :: IO ()
+main = getContents >>= solve
---
+++ src/D05/Solution.hs
@@ -0,0 +1,89 @@
+module D05.Solution where
+
+import AOC.Parser
+import Control.Monad ((>=>))
+import Data.Foldable (fold, foldl')
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes, listToMaybe)
+import Data.Monoid (Endo (..))
+import Text.Megaparsec
+import Text.Megaparsec.Char (char, digitChar, eol, newline, string)
+
+type Index = Int
+
+type Crate = Char
+
+type Stacks = M.Map Index [Crate]
+
+data Move = Move {mvQty :: Int, mvFrom :: Index, mvTo :: Index} deriving (Eq, Show)
+
+type Input = (Stacks, [Move])
+
+performMove :: Move -> Stacks -> Maybe Stacks
+performMove (Move qty from to) = repeatM qty (pop from >=> (return . uncurry (push to)))
+
+push :: Index -> Crate -> Stacks -> Stacks
+push idx crate = flip M.alter idx $ \case
+  Nothing -> Just [crate]
+  Just cs -> Just $ crate : cs
+
+pop :: Index -> Stacks -> Maybe (Crate, Stacks)
+pop idx stacks = case M.lookup idx stacks of
+  Just (c : cs) -> Just (c, M.insert idx cs stacks)
+  _ -> Nothing
+
+repeatM :: Monad m => Int -> (a -> m a) -> a -> m a
+repeatM 0 _ = return
+repeatM n f = f >=> repeatM (n - 1) f
+
+--
+
+solve1 :: Input -> IO ()
+solve1 (stacks, moves) = do
+  let Just stacks' = foldl' (\s move -> s >>= performMove move) (Just stacks) moves
+  print . sequence $ M.foldr' ((:) . listToMaybe) [] stacks'
+
+---
+
+inputP :: Parser Input
+inputP =
+  (,)
+    <$> (stacksP <* anyLineP <* newline)
+    <*> movesP
+    <* eof
+
+stacksP :: Parser Stacks
+stacksP = flip appEndo M.empty . fold <$> cratePushP `sepEndBy` eol
+  where
+    crateP :: Parser (Maybe Crate)
+    crateP = Nothing <$ string "   " <|> Just <$> (char '[' *> anySingle <* char ']')
+
+    cratePushP :: Parser (Endo Stacks)
+    cratePushP = do
+      inserts <- catMaybes . zipWith (fmap . (,)) [1 ..] <$> crateP `sepBy` char ' '
+      return $ foldMap (uncurry pushCrate) inserts
+    pushCrate :: Int -> Crate -> Endo Stacks
+    pushCrate stack crate = Endo $ push stack crate
+
+movesP :: Parser [Move]
+movesP =
+  ( Move
+      <$> (string "move " *> intP)
+      <*> (string " from " *> intP)
+      <*> (string " to " *> intP)
+  )
+    `sepEndBy1` eol
+  where
+    intP :: Parser Int
+    intP = read <$> some digitChar
+
+anyLineP :: Parser String
+anyLineP = manyTill anySingle newline
+
+---
+
+solve :: String -> IO ()
+solve s = do
+  inp <- parseOrFail inputP "input" s
+  solve1 inp
+  -- solve2 inp
