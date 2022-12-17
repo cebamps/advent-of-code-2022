8a30948370cca92bbff5b6e81d226136c183d1aa
Cédric B <cedric@arch.localdomain>
2022-12-17 18:54:05 +0100

    day 16 part 2 with a bug...

--- src/D16/Solution.hs
+++ src/D16/Solution.hs
@@ -3,7 +3,10 @@ module D16.Solution (solve) where
 import AOC.Parser
 import Algorithm.Search (dijkstra)
 import Control.Arrow (Arrow (first), (&&&))
-import Data.List (foldl')
+import Data.Function (on)
+import Data.Functor ((<&>))
+import Data.Functor.Compose (Compose (..))
+import Data.List (foldl', groupBy, sortOn)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Set (Set)
@@ -59,7 +62,7 @@ distances1 inp = distances1' next
 
 -- compute distances using a breadth-first walk
 distances1' :: Ord a => (a -> [a]) -> a -> Map a Int
-distances1' next start = go 0 M.empty [start]
+distances1' next start = go 0 (M.singleton start 0) [start]
   where
     go _ seen [] = seen
     go n seen xs =
@@ -67,34 +70,73 @@ distances1' next start = go 0 M.empty [start]
           xs' = [x' | x <- xs, x' <- next x, not (x' `M.member` seen)]
        in go (n + 1) seen' xs'
 
-data SearchState = SearchState {sT :: Int, sPos :: Name, sOpen :: Set Name} deriving (Eq, Ord, Show)
+data SearchState = SearchState {sT :: Int, sPos :: [Position], sOpen :: Set Name} deriving (Eq, Ord, Show)
 
-initState :: SearchState
-initState = SearchState 0 startNode S.empty
+-- Position in the graph. The distance represents how far away we are from the
+-- node, in an undetermined direction.
+data Position = Position
+  { pId :: Name,
+    pDistance :: Int
+  }
+  deriving (Eq, Ord, Show)
+
+initStateForCount :: Int -> SearchState
+initStateForCount n = SearchState 0 (replicate n (Position startNode 0)) S.empty
 
 startNode :: String
 startNode = "AA"
 
-finalTime :: Int
-finalTime = 30
-
-findPath :: Input -> Maybe (Int, [SearchState])
-findPath inp = first toObjective <$> dijkstra next cost ((== finalTime) . sT) initState
+findPath :: Int -> SearchState -> Input -> Maybe (Int, [SearchState])
+findPath finalTime initState inp = first toObjective <$> dijkstra next cost ((== finalTime) . sT) initState
   where
     inpD = distances inp
     -- next valve targets
     next :: SearchState -> [SearchState]
     next s =
-      case [ SearchState {sPos = pos', sT = t', sOpen = open'}
-             | (pos', d) <- M.toList $ inpD M.! sPos s,
-               pos' `S.notMember` sOpen s,
-               pos' /= startNode, -- don't come back to start
-               let t' = sT s + d + 1, -- walk to destination and open valve
-               let open' = S.insert pos' $ sOpen s,
+      case [ SearchState {sPos = poss', sT = t', sOpen = open'}
+             | (dt, node', _, poss') <- nextMoves (sPos s) keep,
+               let t' = sT s + dt,
+               let open' = S.insert node' $ sOpen s,
                t' <= finalTime
            ] of
         [] -> [s {sT = finalTime}] -- no time to get to another valve, stay put
         x -> x
+      where
+        keep node' =
+          node' `S.notMember` sOpen s
+            && node' /= startNode -- don't come back to start
+
+    -- gives the possible moves along with the valve opened, time elapsed at
+    -- the end of each move, and index of the mover
+    nextMoves :: [Position] -> (Name -> Bool) -> [(Int, Name, Int, [Position])]
+    nextMoves pos keep = prune . concatMap getCompose $ mapsEach (nextMovesOne keep) (zip [0 ..] pos)
+      where
+        -- for moves of different movers that take the same time, only keep one
+        -- to avoid exponential branching
+        prune :: [(Int, Name, Int, [Position])] -> [(Int, Name, Int, [Position])]
+        prune mvs =
+          [ mv
+            | byTime <- groupOn dt mvs,
+              mv <- filter (\x -> idx x == idx (head byTime)) byTime
+          ]
+        dt (x, _, _, _) = x
+        idx (_, _, x, _) = x
+        groupOnSorted f = groupBy ((==) `on` f)
+        groupOn f = groupOnSorted f . sortOn f
+    nextMovesOne ::
+      (Name -> Bool) ->
+      (Int, Position) ->
+      Compose [] ((,,,) Int Name Int) (Position, (Int, Position) -> Position)
+    nextMovesOne keep (idx, pos) =
+      Compose
+        [ (dt, node', idx, (pos', \(_, pother) -> pother {pDistance = pDistance pother + dt}))
+          | (node', d) <- M.toList $ inpD M.! pId pos,
+            let dt = d - pDistance pos + 1 -- walk to destination and open valve
+                pos' = Position node' 0,
+            keep node',
+            dt >= 0
+        ]
+
     -- The relieved pressure as stated in the problem. Valves are only ever
     -- opened on the time right before a destination node.
     objective :: SearchState -> SearchState -> Int
@@ -107,12 +149,27 @@ findPath inp = first toObjective <$> dijkstra next cost ((== finalTime) . sT) in
     -- revert back to objective value
     toObjective x = finalTime * capacity - x
 
+mapsEach :: Functor f => (a -> f (b, a -> b)) -> [a] -> [f [b]]
+mapsEach f = fmap go . splits
+  where
+    splits :: [a] -> [([a], a, [a])]
+    splits [] = []
+    splits (x : xs) = ([], x, xs) : fmap (\(h, m, t) -> (x : h, m, t)) (splits xs)
+
+    go (h, m, t) = f m <&> \(m', g) -> fmap g h <> (m' : fmap g t)
+
 ---
 
-solve1 :: Input -> IO ()
-solve1 inp = case findPath inp of
+solveAny :: (Int, Int) -> Input -> IO ()
+solveAny (t, n) inp = case findPath t (initStateForCount n) inp of
   Nothing -> putStrLn "solution not found"
   Just (p, _) -> print p
+
+solve1 :: Input -> IO ()
+solve1 = solveAny (30, 1)
+
+solve2 :: Input -> IO ()
+solve2 = solveAny (26, 2)
 
 ---
 
@@ -138,3 +195,4 @@ solve :: String -> IO ()
 solve s = do
   inp <- parseOrFail inputP "input" s
   solve1 inp
+  solve2 inp
